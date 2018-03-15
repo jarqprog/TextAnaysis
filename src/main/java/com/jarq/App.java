7ad803c134072b09b52803b5d6805866b50ae5c7
jarqprog <jarqprog@gmail.com>
2018-03-16 00:31:32 +0100

    add analyze method in App class

--- src/main/java/com/jarq/App.java
+++ src/main/java/com/jarq/App.java
@@ -1,37 +1,53 @@
 package com.jarq;
 
 
+import com.jarq.view.View;
+
 import java.io.IOException;
 import java.util.Iterator;
 
 public class App {
     public static void main( String[] args ) {
+        Long start = System.currentTimeMillis();
 
-        try {
-//            IterableText<String> fileContent = new FileContent("test.txt");
-//            Iterator<String> charIterator = fileContent.charIterator();
-//
-//            if(charIterator != null) {
-//                while (charIterator.hasNext()) {
-//                    String character = charIterator.next();
-//                    System.out.println(character);
-//                }
-//            }
-
-            IterableText<String> fileContent1 = new FileContent("test2.txt");
-            Iterator<String> wordIt = fileContent1.wordIterator();
-
-            if(wordIt != null) {
-                while (wordIt.hasNext()) {
-                    String word = wordIt.next();
-                    System.out.println(word);
-                }
-            }
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
+        for(String filename : args) {
+            analyzeTextFromFile(filename);
         }
 
+        Double benchmark = ((System.currentTimeMillis() - start)*0.001);
+        View.print("Benchmark time: " + benchmark + " secs");
+    }
 
-        System.out.println( "Hello World!" );
+    private static void analyzeTextFromFile(String filename) {
+
+        FileContent fileContent = null;
+        try {
+            fileContent = new FileContent(filename);
+        } catch (IOException e) {
+            View.print(e.getMessage());
+            System.exit(1);
+        }
+        Iterator<String> charIterator = fileContent.charIterator();
+        Iterator<String> wordIterator = fileContent.wordIterator();
+        StatisticalAnalysis charAnalysis = new StatisticalAnalysis(charIterator);
+        StatisticalAnalysis wordAnalysis = new StatisticalAnalysis(wordIterator);
+        analyzeCharacters(charAnalysis);
+        analyzeWords(wordAnalysis);
+    }
+
+    private static void analyzeCharacters(StatisticalAnalysis charAnalysis) {
+        if(! charAnalysis.hasCharIterator()) {
+            throw new IllegalArgumentException("You should use analysis with CharIterator!");
+        }
+        View.print("Char count: " + charAnalysis.size());
+    }
+
+    private static void analyzeWords(StatisticalAnalysis wordAnalysis) {
+        if(wordAnalysis.hasCharIterator()) {
+            throw new IllegalArgumentException("You should use analysis with WordIterator!");
+        }
+        View.print("Word count: " + wordAnalysis.size());
+        View.print("Dict size: " + wordAnalysis.dictionarySize());
+        View.print(wordAnalysis.getDictionary());
     }
 }
