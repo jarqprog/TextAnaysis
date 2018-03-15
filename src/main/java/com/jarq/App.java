f1d2d9d7440d8e4eb198778aabd4125bdb4c0e46
jarqprog <jarqprog@gmail.com>
2018-03-15 22:25:47 +0100

    implement test code in App

--- src/main/java/com/jarq/App.java
+++ src/main/java/com/jarq/App.java
@@ -1,22 +1,36 @@
 package com.jarq;
 
-import com.jarq.iterators.CharIterator;
 
 import java.io.IOException;
 import java.util.Iterator;
 
 public class App {
-    public static void main( String[] args ) throws IOException {
+    public static void main( String[] args ) {
 
-        IterableText<String> fileContent = new FileContent("test.txt");
-        Iterator<String> charIterator = fileContent.charIterator();
+        try {
+//            IterableText<String> fileContent = new FileContent("test.txt");
+//            Iterator<String> charIterator = fileContent.charIterator();
+//
+//            if(charIterator != null) {
+//                while (charIterator.hasNext()) {
+//                    String character = charIterator.next();
+//                    System.out.println(character);
+//                }
+//            }
 
-        if(charIterator != null) {
-            while (charIterator.hasNext()) {
-                String line = charIterator.next();
-                System.out.println(line);
+            IterableText<String> fileContent1 = new FileContent("test2.txt");
+            Iterator<String> wordIt = fileContent1.wordIterator();
+
+            if(wordIt != null) {
+                while (wordIt.hasNext()) {
+                    String word = wordIt.next();
+                    System.out.println(word);
+                }
             }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
+
 
         System.out.println( "Hello World!" );
     }
